c472ba081cd9b8f9a3525c28eb48bfd97557e623
Loïc Chagnoleau <furlings@pm.me>
2026-08-21 10:50:53 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move bloc to the up" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move bloc to the down" })
 vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { desc = "[R]eplace current [W]ord" })
 
-vim.keymap.set("n", "<leader>ho", ":%!xxd<CR>:set ft=xxd<CR>", { desc = "[H]ex mode [O]pen" })
-vim.keymap.set("n", "<leader>hc", ":%!xxd -r<CR>", { desc = "[H]ex mode [C]lose" })
+-- vim.keymap.set("n", "<leader>ho", ":%!xxd<CR>:set ft=xxd<CR>", { desc = "[H]ex mode [O]pen" })
+-- vim.keymap.set("n", "<leader>hc", ":%!xxd -r<CR>", { desc = "[H]ex mode [C]lose" })
 -- vim.keymap.set("n", "<leader>st", builtin.treesitter, { desc = "Treesitter" })
